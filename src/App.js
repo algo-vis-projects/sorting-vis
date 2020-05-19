547777f813c43906e8bc4dc753f0d2fa5c01ce50
Ryan Kerins <ryanakerins@gmail.com>
2020-05-19 14:19:34 -0400

    initial project structure, react router, prettier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link, Route } from 'react-router-dom';
 import './App.css';
+import { Merge, Insertion, Bubble } from './SortingAlgorithms';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+        <Link to="/merge">Merge</Link>
+        <Link to="/insertion">Insertion</Link>
+        <Link to="/bubble">Bubble</Link>
+      </div>
+      <Route path="/merge" component={Merge} />
+      <Route path="/insertion" component={Insertion} />
+      <Route path="/bubble" component={Bubble} />
     </div>
   );
 }
---
+++ src/SortingAlgorithms/index.js
@@ -0,0 +1,5 @@
+import Bubble from './Bubble';
+import Insertion from './Insertion';
+import Merge from './Merge';
+
+export { Merge, Insertion, Bubble };
