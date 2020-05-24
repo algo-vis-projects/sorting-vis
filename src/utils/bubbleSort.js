e22d3aecc7540432b3910a77c99f55ad18217dca
Ryan Kerins <ryanakerins@gmail.com>
2020-05-24 17:27:24 -0400

    colors change for swapping elements

--- src/utils/bubbleSort.js
+++ src/utils/bubbleSort.js
@@ -1,16 +1,16 @@
 const bubbleSort = (arr) => {
-  let history = [[...arr]];
+  let history = [{ currSort: [...arr], swap1: null, swap2: null }];
 
   for (let i = 0; i < arr.length - 1; ++i) {
     let swapped = false;
     for (let j = 0; j < arr.length - i - 1; ++j) {
-      const lastArr = history[history.length - 1];
+      const lastArr = history[history.length - 1].currSort;
       let copy = lastArr.map((ele) => ({ ...ele }));
       if (copy[j].value > copy[j + 1].value) {
         [copy[j], copy[j + 1]] = [copy[j + 1], copy[j]];
         swapped = true;
 
-        history.push(copy);
+        history.push({ currSort: copy, swap1: j, swap2: j + 1 });
       }
     }
 
--- src/utils/numsToArray.js
+++ src/utils/numsToArray.js
@@ -1,11 +1,7 @@
 const numsToArray = (nums) => {
-  const numsStringArr = nums.toString().split('');
-  let numsArr = [];
-  numsStringArr.forEach((num) => numsArr.push(Number(num)));
-  console.log(numsArr);
   let arrayOfObjs = [];
 
-  numsArr.forEach((num, idx) =>
+  nums.forEach((num, idx) =>
     arrayOfObjs.push({
       label: idx,
       value: num,
